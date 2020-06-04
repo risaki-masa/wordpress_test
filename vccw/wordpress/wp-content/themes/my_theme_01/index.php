e86bed91dff281542139787e2e109811c59ec3ca
Risaki <kyozyu_masa@yahoo.co.jp>
2020-06-04 22:52:37 +0900

    テーマに必要な最小のファイルを追加

---
+++ vccw/wordpress/wp-content/themes/my_theme_01/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>タイトル</title>
+</head>
+<body>
+    <p>あああ</p>
+</body>
+</html>
